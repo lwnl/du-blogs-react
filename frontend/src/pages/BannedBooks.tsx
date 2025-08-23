f81ea11be835c3a0aa67c661b4e894732060118a
lwnl <liangwang.fr@gmail.com>
2025-08-23 11:19:36 +0200

    update

--- frontend/src/pages/BannedBooks.tsx
+++ frontend/src/pages/BannedBooks.tsx
@@ -4,9 +4,6 @@ import axios from "axios";
 import { useAuthCheck } from "../hooks/useAuthCheck";
 
 const BannedBooks = () => {
-  const [expanded, setExpanded] = useState(false);
-  const handleToggle = () => setExpanded(!expanded);
-
   interface IBannedBook {
     _id: string;
     bookName: string;
@@ -20,6 +17,7 @@ const BannedBooks = () => {
     updatedAt: Date;
   }
 
+  const [currentBookId, setCurrentBookId] = useState<string | null>(null);
   const { HOST, authenticated, user } = useAuthCheck();
   const [loading, setLoading] = useState(true); // <-- 新增 loading 状态
   const [error, setError] = useState<string | null>(null); // <-- 错误状态
@@ -51,68 +49,73 @@ const BannedBooks = () => {
   return (
     <div className="BannedBooks">
       <ul>
-        {bannedBooks?.map((book) => (
-          <li key={book._id} className="book-item">
-            <div className="book-cover">
-              <div>
-                <img src={book.coverLink} alt={book.bookName} />
-              </div>
-              <p className="book-intro flex-column">
-                <span className="book-name">{book.bookName}</span>
-                <span>文件格式：{book.format}</span>
-                <a href={book.downloadLink} target="_blank">
-                  <span>下载</span>
-                </a>
-              </p>
-            </div>
-            <div className="book-info flex-column">
-              <div className="book-review">
-                <div className="book-stars">
-                  <img
-                    src="https://storage.googleapis.com/daniel-jansen7879-bucket-1/projects/my-blog/images/in-books/star_null.gif"
-                    alt="star-null"
-                  />
-                  <img
-                    src="https://storage.googleapis.com/daniel-jansen7879-bucket-1/projects/my-blog/images/in-books/star_null.gif"
-                    alt="star-null"
-                  />
-                  <img
-                    src="https://storage.googleapis.com/daniel-jansen7879-bucket-1/projects/my-blog/images/in-books/star_null.gif"
-                    alt="star-null"
-                  />
-                  <img
-                    src="https://storage.googleapis.com/daniel-jansen7879-bucket-1/projects/my-blog/images/in-books/star_null.gif"
-                    alt="star-null"
-                  />
-                  <img
-                    src="https://storage.googleapis.com/daniel-jansen7879-bucket-1/projects/my-blog/images/in-books/star_null.gif"
-                    alt="star-null"
-                  />
+        {bannedBooks?.map((book) => {
+          const isExpanded = currentBookId === book._id;
+          return (
+            <li key={book._id} className="book-item">
+              <div className="book-cover">
+                <div>
+                  <img src={book.coverLink} alt={book.bookName} />
                 </div>
-                <p>
-                  <span>0 </span>分
-                </p>
-                <p>
-                  <span>0 </span>人评价
+                <p className="book-intro flex-column">
+                  <span className="book-name">{book.bookName}</span>
+                  <span>文件格式：{book.format}</span>
+                  <a href={book.downloadLink} target="_blank">
+                    <span>下载</span>
+                  </a>
                 </p>
               </div>
+              <div className="book-info flex-column">
+                <div className="book-review">
+                  <div className="book-stars">
+                    <img
+                      src="https://storage.googleapis.com/daniel-jansen7879-bucket-1/projects/my-blog/images/in-books/star_null.gif"
+                      alt="star-null"
+                    />
+                    <img
+                      src="https://storage.googleapis.com/daniel-jansen7879-bucket-1/projects/my-blog/images/in-books/star_null.gif"
+                      alt="star-null"
+                    />
+                    <img
+                      src="https://storage.googleapis.com/daniel-jansen7879-bucket-1/projects/my-blog/images/in-books/star_null.gif"
+                      alt="star-null"
+                    />
+                    <img
+                      src="https://storage.googleapis.com/daniel-jansen7879-bucket-1/projects/my-blog/images/in-books/star_null.gif"
+                      alt="star-null"
+                    />
+                    <img
+                      src="https://storage.googleapis.com/daniel-jansen7879-bucket-1/projects/my-blog/images/in-books/star_null.gif"
+                      alt="star-null"
+                    />
+                  </div>
+                  <p>
+                    <span>0 </span>分
+                  </p>
+                  <p>
+                    <span>0 </span>人评价
+                  </p>
+                </div>
 
-              <article className="flex-column">
-                <div
-                  dangerouslySetInnerHTML={{ __html: book.summary }}
-                  className={
-                    expanded ? "book-summary expanded" : "book-summary"
-                  }
-                ></div>
-                {expanded ? (
-                  <button onClick={handleToggle}>收起</button>
-                ) : (
-                  <button onClick={handleToggle}>阅读更多</button>
-                )}
-              </article>
-            </div>
-          </li>
-        ))}
+                <article className="flex-column">
+                  <div
+                    dangerouslySetInnerHTML={{ __html: book.summary }}
+                    className={
+                      isExpanded ? "book-summary expanded" : "book-summary"
+                    }
+                  ></div>
+                  {isExpanded ? (
+                    <button onClick={() => setCurrentBookId(null)}>收起</button>
+                  ) : (
+                    <button onClick={() => setCurrentBookId(book._id)}>
+                      阅读更多
+                    </button>
+                  )}
+                </article>
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
