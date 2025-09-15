3687a0e7babd7f9bfc0bb6a876a6ce1bee414408
lwnl <liangwang.fr@gmail.com>
2025-09-15 11:15:23 +0200

    修改News模型，添加user field

---
+++ backend/models/News.ts
@@ -0,0 +1,47 @@
+import mongoose, { Schema, Model, Document } from 'mongoose'
+import { jsonDateTransform } from '../utils/formatDate';
+
+export interface INews extends Document {
+  title: string,
+  content: string,
+  user: string,
+  source: string,
+  comments: string[],
+  createdAt: Date,
+  updatedAt: Date
+}
+
+const NewsSchema: Schema<INews> = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  user: {
+    type: String,
+    required: true
+  },
+  source: {
+    type: String,
+    required: true
+  },
+  comments: {
+    type: [String]
+  },
+}, {
+  timestamps: {
+    createdAt: true,
+    updateAt:true
+  }
+})
+
+NewsSchema.set('toJSON', {
+  transform: jsonDateTransform
+});
+
+const News: Model<INews> = mongoose.models.News || mongoose.model<INews>('News', NewsSchema)
+
+export default News
